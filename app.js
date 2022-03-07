cc79a93ba50a8196c35314a4c71a9a2edf4f3f14
DannyWong <danny.96.wong@gmail.com>
2022-03-07 12:16:55 -0800

    created grocery list and added embolden and crossout features to list items

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false,
+      bold: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onMouseover(e) {
+    this.setState({bold: true})
+  }
+
+  onMouseout(e) {
+    this.setState({bold: false})
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style}
+        onClick={this.onListItemClick.bind(this)}
+        onMouseEnter={this.onMouseover.bind(this)}
+        onMouseLeave={this.onMouseout.bind(this)}>{this.props.grocery}</li>
+      // <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.grocery}</li>
+    )
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.grocerys.map(grocery =>
+      <GroceryListItem grocery={grocery} />
+    )}
+  </ul>
+)
+
+var App = () => (
+  <div>
+    <h2>This is a Grocery List</h2>
+    <GroceryList grocerys={['Cookies', 'Milk', 'Bread']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
